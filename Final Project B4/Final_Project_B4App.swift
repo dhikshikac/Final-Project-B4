ac128866014a2b4964bc8392b3444cb36194da62
Dhikshika C <dhikshika.cherivirala@gmail.com>
2024-07-26 13:14:59 -0400

    Initial Commit

---
+++ Final Project B4/Final_Project_B4App.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_B4App.swift
+//  Final Project B4
+//
+//  Created by scholar on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_B4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
