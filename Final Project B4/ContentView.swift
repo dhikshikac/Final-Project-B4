86e9ede2238403e1f6b3ee430e5052820fd98443
Dhikshika C <dhikshika.cherivirala@gmail.com>
2024-07-30 11:27:57 -0400

    added the color values in a comment

--- Final Project B4/ContentView.swift
+++ Final Project B4/ContentView.swift
@@ -5,6 +5,9 @@
 //  Created by scholar on 7/26/24.
 //
 
+//Color(red: 0.72, green: 0.75, blue: 0.66) - light green
+//Color(red: 0.15, green: 0.33, blue: 0.24) - dark green
+
 import SwiftUI
 
 struct ContentView: View {
