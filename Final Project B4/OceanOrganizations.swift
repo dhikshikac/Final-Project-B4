7bc94cccb1f477433e23e43cb49bd22ec2b52609
Dhikshika C <dhikshika.cherivirala@gmail.com>
2024-07-31 11:27:30 -0400

    home page is basically done

---
+++ Final Project B4/OceanOrganizations.swift
@@ -0,0 +1,18 @@
+//
+//  OceanOrganizations.swift
+//  Final Project B4
+//
+//  Created by scholar on 7/31/24.
+//
+
+import SwiftUI
+
+struct OceanOrganizations: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OceanOrganizations()
+}
